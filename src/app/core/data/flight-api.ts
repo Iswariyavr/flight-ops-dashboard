0e9485301aa9c122631420ea2096aa1e2f6e0adc
iswariyav <ishuvr2708@gmail.com>
2026-09-24 22:07:26 +0530

    add flight models, mock data and FlightApiService

--- src/app/core/data/flight-api.ts
+++ src/app/core/data/flight-api.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, shareReplay } from 'rxjs';
+import { Airport, Flight } from '../models/flight.model';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class FlightApi {}
+@Injectable({ providedIn: 'root' })
+export class FlightApiService {
+  private http = inject(HttpClient);
+
+  /** Loaded once, then shared by everyone who subscribes. */
+  readonly flights$: Observable<Flight[]> = this.http
+    .get<Flight[]>('data/flights.json')
+    .pipe(shareReplay(1));
+
+  readonly airports$: Observable<Airport[]> = this.http
+    .get<Airport[]>('data/airports.json')
+    .pipe(shareReplay(1));
+}
