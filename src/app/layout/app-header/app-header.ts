ca29e82d6a30f9cb8dd1c490d2cf8eedd2c7ec65
iswariyav <ishuvr2708@gmail.com>
2026-09-24 23:24:57 +0530

    add design tokens, routing and three-column operations shell

--- src/app/layout/app-header/app-header.ts
+++ src/app/layout/app-header/app-header.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-app-header',
-  imports: [],
   templateUrl: './app-header.html',
   styleUrl: './app-header.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppHeader {}
